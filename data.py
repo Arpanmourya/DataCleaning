e673de501b3981204374dcbedc5474829c2041af
Amitabh <hisamit97@gmail.com>
2024-10-21 12:23:32 +0530

    added new files

---
+++ data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
+df = pd.read_csv('WA_Fn-UseC_-Telco-Customer-Churn.csv')
+print(df)
+df.shape
+df.info()
